8b93a956022e2f4b0d35eae5eb34f59ca44e532f
n4dch4gz4c-hue <n4dch4gz4c@privaterelay.appleid.com>
2026-09-15 01:50:42 +0300

    Onbellek temizle v40

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const VER="v32";
-self.addEventListener("install",function(e){
-  self.skipWaiting();
-});
+const VER="v40";
+self.addEventListener("install",function(e){self.skipWaiting();});
 self.addEventListener("activate",function(e){
   e.waitUntil(caches.keys().then(function(keys){
     return Promise.all(keys.map(function(k){return caches.delete(k);}));
   }).then(function(){return self.clients.claim();}));
 });
 self.addEventListener("fetch",function(e){
-  var u=e.request.url;
-  if(u.indexOf("cadde-data.json")!==-1 || u.indexOf("api.github.com")!==-1){
-    e.respondWith(fetch(e.request,{cache:"no-store"}));
-    return;
-  }
-  e.respondWith(fetch(e.request,{cache:"no-store"}).catch(function(){
-    return caches.match(e.request);
-  }));
+  e.respondWith(fetch(e.request,{cache:"no-store"}).catch(function(){return caches.match(e.request);}));
 });
